70a8c1593f3619aab1692f2d7d2f66445309a53f
ka <ka.kaosf@gmail.com>
2015-08-12 00:08:46 +0900

    Do data driven test
    ref. http://qiita.com/repeatedly/items/727b08599d87af7fa671

--- test/test_color-schema-validator.rb
+++ test/test_color-schema-validator.rb
@@ -18,13 +18,56 @@ class TestColorSchemaValidator < Test::Unit::TestCase
     @instance = model_class.new(:model)
   end
 
-  test "valid color schema strings" do
-    @instance.color = '#012345'
+  data(
+    '6 hex 1'       => '#012345',
+    '6 hex 2'       => '#6789AB',
+    '6 hex 3'       => '#CDEF01',
+    '6 hex 2 lower' => '#6789ab',
+    '6 hex 3 lower' => '#cdef01',
+    '3 hex 1'       => '#012',
+    '3 hex 2'       => '#345',
+    '3 hex 3'       => '#678',
+    '3 hex 4'       => '#9AB',
+    '3 hex 5'       => '#CDE',
+    '3 hex 6'       => '#F01',
+    '3 hex 4 lower' => '#9ab',
+    '3 hex 5 lower' => '#cde',
+    '3 hex 6 lower' => '#f01',
+    '0'             => '#000000',
+    '1'             => '#111111',
+    '2'             => '#222222',
+    '3'             => '#333333',
+    '4'             => '#444444',
+    '5'             => '#555555',
+    '6'             => '#666666',
+    '7'             => '#777777',
+    '8'             => '#888888',
+    'A'             => '#AAAAAA',
+    'B'             => '#BBBBBB',
+    'C'             => '#CCCCCC',
+    'D'             => '#DDDDDD',
+    'E'             => '#EEEEEE',
+    'F'             => '#FFFFFF',
+    'a'             => '#aaaaaa',
+    'b'             => '#bbbbbb',
+    'c'             => '#cccccc',
+    'd'             => '#dddddd',
+    'e'             => '#eeeeee',
+    'f'             => '#ffffff')
+  test "valid color schema strings" do |data|
+    @instance.color = data
     assert_true @instance.valid?
   end
 
-  test "invalid color schema strings" do
-    @instance.color = ''
+  data(
+    'empty'        => '',
+    'without #'    => '012345',
+    '4 characters' => '#0123',
+    '5 characters' => '#01234',
+    '7 characters' => '#0123456',
+    'not hex'      => '#01234g')
+  test "invalid color schema strings" do |data|
+    @instance.color = data
     assert_false @instance.valid?
   end
 end
