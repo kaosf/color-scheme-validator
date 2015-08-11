6f6be18db2de1b4a9b41976465385905e10c866f
ka <ka.kaosf@gmail.com>
2015-08-12 00:03:36 +0900

    Test with ActiveModel
    ref. http://qiita.com/r7kamura/items/70c22300499b795af19c

--- test/test_color-schema-validator.rb
+++ test/test_color-schema-validator.rb
@@ -3,7 +3,28 @@ require 'active_model'
 require 'color-schema-validator'
 
 class TestColorSchemaValidator < Test::Unit::TestCase
-  test "" do
-    assert true
+  setup do
+    model_class = Struct.new(:model) do
+      include ActiveModel::Validations
+
+      def self.name
+        'Model'
+      end
+
+      attr_accessor :color
+      validates :color, color_schema: true
+    end
+
+    @instance = model_class.new(:model)
+  end
+
+  test "valid color schema strings" do
+    @instance.color = '#012345'
+    assert_true @instance.valid?
+  end
+
+  test "invalid color schema strings" do
+    @instance.color = ''
+    assert_false @instance.valid?
   end
 end
