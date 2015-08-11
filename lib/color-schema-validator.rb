0f946ceb7774968446a102e71411dea54ac482e3
ka <ka.kaosf@gmail.com>
2015-08-12 00:54:50 +0900

    Fix regexp to more strong
    According to the following error on Rails 4:
    
        ArgumentError: The provided regular expression is using multiline anchors (^ or $), which may present a security risk. Did you mean to use \A and \z, or forgot to add the :multiline => true option?
        from /some/where/vendor/bundle/ruby/2.2.0/gems/activemodel-4.2.3/lib/active_model/validations/format.rb:39:in `check_options_validity'

--- lib/color-schema-validator.rb
+++ lib/color-schema-validator.rb
@@ -1,6 +1,6 @@
 class ColorSchemaValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^#(\h{6}|\h{3})$/
+    unless value =~ /\A#(\h{6}|\h{3})\z/
       record.errors.add(attribute, options[:message] || :invalid)
     end
   end
