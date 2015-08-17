eb345a399855ef0d1e7d64a618aee29ee801f186
ka <ka.kaosf@gmail.com>
2015-08-17 22:54:08 +0900

    Create validation error messages

--- lib/color-schema-validator.rb
+++ lib/color-schema-validator.rb
@@ -1,7 +1,7 @@
 class ColorSchemaValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A#(\h{6}|\h{3})\z/
-      record.errors.add(attribute, options[:message] || :invalid)
+      record.errors.add(attribute, options[:message] || :invalid_color_schema)
     end
   end
 end
